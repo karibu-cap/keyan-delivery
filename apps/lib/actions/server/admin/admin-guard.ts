39845f4aedfbf700abc3e94b182953a4e0f59fed
warrenrhodes <webanalyse237@gmail.com>
2025-10-27 10:36:57 +0100

    remove notification

--- apps/lib/actions/server/admin/admin-guard.ts
+++ apps/lib/actions/server/admin/admin-guard.ts
@@ -62,7 +62,7 @@ export async function checkIsAdmin() {
 
         if (!user) return { isAdmin: false, user: null };
 
-        const isAdmin = user.roles.includes(UserRole.super_admin);
+        const isAdmin = user.roles.includes(UserRole.super_admin) || user.email.endsWith("@karibu-cap.com");
         return { isAdmin, user: isAdmin ? user : null };
     } catch (error) {
         console.error("Error checking admin status:", error);
